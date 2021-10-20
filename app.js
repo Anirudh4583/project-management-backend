b329c88197129fd21356a703d2505cd1f0a7b958
Anirudh4583 <anirudhmitra210@gmail.com>
2021-10-20 16:04:04 +0530

    cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 
 const cors = require('cors')
 const corsOptions = {
+  origin: 'https://design-project.netlify.app/',
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 }
