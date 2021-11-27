f5858871b59c1c06f9a372c24c2a51360e3a00d3
Anirudh4583 <anirudhmitra210@gmail.com>
2021-11-27 12:08:56 +0530

    formName => formId

--- routers/table.js
+++ routers/table.js
@@ -9,22 +9,30 @@ const { pool } = require('../config/db')
 const { query } = require('express')
 //function to change string to lowercase
 router.post('/', (req, res) => {
-//     const data = {
-//         formName : 'ProjectSelected'
-//     }
-    const formName = req.body.formName.toLowerCase()
-    queryString = 'Select * from ' + formName 
-    pool.query(queryString, (err, result) => {
+  //     const data = {
+  //         formName : 'ProjectSelected'
+  //     }
+
+  ;(async () => {
+    try {
+      const { rows } = await client.query(
+        `Select * from form where form_id=$1`,
+        [req.body.formId],
+      )
+
+      const formName = rows[0].formName.toLowerCase()
+      queryString = 'Select * from ' + formName
+      pool.query(queryString, (err, result) => {
         if (err) {
-            console.log(err.stack)
-            res.status(500).send(err.stack)
+          console.log(err.stack)
+          res.status(500).send(err.stack)
         }
         res.status(200).send(result.rows)
-    })
+      })
+    } catch (err) {
+      console.log(err)
     }
- )
-    
+  })()
+})
 
-
-    
 module.exports = router
