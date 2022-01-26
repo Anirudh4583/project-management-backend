8a597b52b387f3c9b3333723040f41560ae99591
Dev Parmar <61654319+DevParmar1@users.noreply.github.com>
2022-01-26 19:26:01 +0530

    Update file.js

--- routers/file.js
+++ routers/file.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require("../controllers/file.controller");
 
   router.post("/upload", controller.upload);
-  router.get("/files", controller.getListFiles);
+  router.get("/file", controller.getListFiles);
   router.get("/files/:name", controller.download);
 
 
